6ecf3677d0c749fbe034b187b52ec503d3f1736b
yhzhan <yhzhan@juniper.net>
2014-05-08 19:15:00 -0700

    JUNIPER-ONLY: PL-1015, ssh to device via Topology

---
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.netutils/src/main/java/org/opennms/features/topology/netutils/internal/SshWindow.java
@@ -0,0 +1,48 @@
+package org.opennms.features.topology.netutils.internal;
+
+import java.net.URL;
+
+import org.opennms.features.topology.api.support.InfoWindow;
+
+/**
+ * The SshWindow class constructs a custom Window component that contains an embedded
+ * browser displaying the SSH information of the currently selected node
+ * @author Leonardo Bell
+ * @author Philip Grenon
+ * @version 1.0
+ */
+public class SshWindow extends InfoWindow {
+
+  private static final long serialVersionUID = -9008855502553868301L;
+
+  /**
+   * Label given to vertexes that have no real label.
+   */
+  private static final String NO_LABEL_TEXT = "no such label";
+
+  /**
+     * The SshWindow method constructs a sub-window instance which can be added to a main window.
+     * The sub-window contains an embedded browser which displays the Node Info page of the currently selected
+     * node.
+     * @param node Selected node
+     * @param width Width of the main window
+     * @param height Height of the main window
+     */
+   public SshWindow(final Node node, final URL nodeURL) {
+     super(nodeURL, new LabelCreator() {
+
+      @Override
+      public String getLabel() {
+        String label = node == null ? "" : node.getLabel();
+
+            /*Sets up window settings*/
+            if (label == null || label.equals("") || label.equalsIgnoreCase(NO_LABEL_TEXT)) {
+                label = "";
+            } else {
+              label = " To " + label;
+            }
+            return "SSH" + label;
+      }
+    });
+   }
+}
