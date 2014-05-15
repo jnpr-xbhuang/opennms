144e7df2af6a18ffbda843a7b4032ab5c9794151
yhzhan <yhzhan@juniper.net>
2014-05-14 20:15:39 -0700

    JUNIPER ONLY: merge some changes from 133R2 to 14.1 <0409-0507>

--- features/notifications/alarm-notification/src/main/etc/alarm-notification/scripts/SDG_App_Script.sh
+++ features/notifications/alarm-notification/src/main/etc/alarm-notification/scripts/SDG_App_Script.sh
@@ -1,7 +1,16 @@
 echo "SDG alarm notification script started "
 logger SDG alarm notification script started
 xml=$1
-echo "Alarm xml being sent is " $xml
-logger Alarm xml sent $xml
-curl -v -u super:juniper123 -X POST -H "Content-type:application/xml" -d "$xml" "http://jmp-cluster:8080/api/sgd/fault/sdg-fault/sdgalarm"
-
+spcialNodeCount=`grep -ce "spaceCluster-VIP" /etc/hosts`
+if [ $spcialNodeCount -gt 0 ];then
+  spaceClusterIp=`awk '/spaceCluster-VIP/{print $1}' /etc/hosts`
+  ipEntryCount=`/etc/init.d/iptables status|grep -ce $spaceClusterIp`
+  if [ $ipEntryCount -eq 0 ];then
+     iptables -I OUTPUT 1 -d spaceCluster-VIP -p all -j ACCEPT
+     logger IP Table entry added
+  fi
+  curl -k -v -u super:juniper123 -X POST -H "Content-type:application/xml" -d "$xml" "https://spaceCluster-VIP/api/sgd/fault/sdg-fault/sdgalarm"
+else
+  curl -v -u \$\$\$rest: -X POST -H "Content-type:application/xml" -d "$xml" "http://jmp-cluster:8080/api/sgd/fault/sdg-fault/sdgalarm"
+  logger Alarm xml sent $xml
+fi
